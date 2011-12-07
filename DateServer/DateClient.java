b42410ba1c509c0bc108ddddaa0e9e2633b0c73b
Juan A. de la Puente <jpuente@dit.upm.es>
2011-12-07 17:47:45 +0100

    just a test

--- DateServer/DateClient.java
+++ DateServer/DateClient.java
@@ -2,21 +2,21 @@ import java.net.*;
 import java.io.*;
 public class DateClient
 {
-    public static void main(String[] args) throws IOException {
-	InputStream in = null;
-	BufferedReader bin = null;
-	Socket sock = null;
-	try {
-	    sock = new Socket("127.0.0.1",6013);
-	    in = sock.getInputStream();
-	    bin = new BufferedReader(new InputStreamReader(in));
-	    String line;
-	    while( (line = bin.readLine()) != null)
-		System.out.println(line);
-	} catch (IOException ioe) {
-	    System.err.println(ioe);
-	} finally {
-            sock.close();
+	public static void main(String[] args) throws IOException {
+		InputStream in = null;
+		BufferedReader bin = null;
+		Socket sock = null;
+		try {
+			sock = new Socket("127.0.0.1",6013);
+			in = sock.getInputStream();
+			bin = new BufferedReader(new InputStreamReader(in));
+			String line;
+			while( (line = bin.readLine()) != null)
+				System.out.println(line);
+		} catch (IOException ioe) {
+			System.err.println(ioe);
+		} finally {
+			sock.close();
+		}
 	}
-    }
 }
--- DateServer/DateServer.java
+++ DateServer/DateServer.java
@@ -2,22 +2,22 @@ import java.net.*;
 import java.io.*;
 public class DateServer
 {
-    public static void main(String[] args) {
-	try {
-	    ServerSocket sock = new ServerSocket(6013);
-	    // now listen for connections
-	    while (true) {
-		Socket client = sock.accept();
-		// we have a connection
-		PrintWriter pout = new 
-		    PrintWriter(client.getOutputStream(), true);
-		// write the Date to the socket
-		pout.println(new java.util.Date().toString());
-		client.close();
-	    }
+	public static void main(String[] args) {
+		try {
+			ServerSocket sock = new ServerSocket(6013);
+			// now listen for connections
+			while (true) {
+				Socket client = sock.accept();
+				// we have a connection
+				PrintWriter pout = new 
+				PrintWriter(client.getOutputStream(), true);
+				// write the Date to the socket
+				pout.println(new java.util.Date().toString());
+				client.close();
+			}
+		}
+		catch (IOException ioe) {
+			System.err.println(ioe);
+		}
 	}
-	catch (IOException ioe) {
-	    System.err.println(ioe);
-	}
-    }
 }
